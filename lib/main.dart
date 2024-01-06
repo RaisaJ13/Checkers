d085b9480eaf8758d6555366ba2f0462f1fe080e
001sara001 <malihatasnim530@gmail.com>
2024-01-06 17:37:50 +0600

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:untitled1/screens/welcome_screen.dart';
 import 'package:untitled1/splash.dart';
-Future<void> main() async {
+import "package:untitled1/firebase_options.dart";
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options:DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
