dbdd58b252684d77fe6d38f254857101dc7b1a1b
RaisaJ13 <152383228+RaisaJ13@users.noreply.github.com>
2024-01-07 15:52:02 +0600

    Update MyDrawerr.dart
    my drawer, 
    button use in diff way

--- lib/page/MyDrawerr.dart
+++ lib/page/MyDrawerr.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:untitled1/page/MenuPage.dart';
+import 'package:untitled1/page/myList.dart';
 import 'package:untitled1/page/profile_page.dart';
-
-import '../screens/applogo.dart';
-import 'MenuPage.dart';
+import 'package:untitled1/screens/applogo.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
@@ -11,63 +11,32 @@ class MyDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       backgroundColor: Colors.black54,
-      child : Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Column(
-          children: [
-            // logo
-            const applogo(),
-
-            //for edit profile
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ProfileScreen()),
-
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.transparent,
-                elevation: 0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  side: BorderSide(color: Colors.white),
-                ),
-              ),
-              child: Container(
-                width: 120,
-                height: 50,
-                alignment: Alignment.center,
-                child: Text('Edit Profile', style: TextStyle(fontSize: 20)),
-              ),
+      child: Column(
+        children: [
+          //header
+            const DrawerHeader(
+                child: applogo(),
             ),
-            //logout
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MenuPage()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.transparent,
-                elevation: 0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  side: BorderSide(color: Colors.white),
-                ),
-              ),
-              child: Container(
-                width: 120,
-                height: 50,
-                alignment: Alignment.center,
-                child: Text('Log Out', style: TextStyle(fontSize: 20)),
-              ),
-            ),
-          ],
-        ),
+          //home list
+           MyListTitle(icon: Icons.home, text: 'Home', onTap: ()=>Navigator.pop(context),),
+          //profile list
+          MyListTitle(icon: Icons.person, text: 'Profile',
+              onTap: ()=>Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context)=>ProfileScreen()),
+              )),
+        MyListTitle(icon: Icons.logout, text: 'Log Out',
+         onTap: ()=>Navigator.push(
+          context,
+          MaterialPageRoute(
+          builder: (context)=>MenuPage()),
+         )),
+          //logout
+
+
+        ],
       ),
     );
-  }
-}
+  }}
+
